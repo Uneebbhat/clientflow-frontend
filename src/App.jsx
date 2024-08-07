373f2f741b875cf7285ffd648cfcdb5852bdfbd4
Uneeb <uneebbhatti3@gmail.com>
2024-08-07 22:10:27 +0500

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello world!</h1>
+        <Router>
+          <Routes></Routes>
+        </Router>
       </div>
     </>
   );
